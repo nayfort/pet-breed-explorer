62dc4847cacae69c7b82f9198d8493689213af7c
nayfort <mitrofanovvladislav2004@gmail.com>
2024-08-08 19:44:06 +0300

    remove deploy errs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+    reactStrictMode: true,
+};
 
 export default nextConfig;
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,16 +1,15 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
   );
 }
